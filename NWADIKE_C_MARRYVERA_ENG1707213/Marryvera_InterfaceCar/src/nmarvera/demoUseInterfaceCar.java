b740b414e40837fcac9d590793371ae194b93fd8
nmarvera123 <86147379+nmarvera123@users.noreply.github.com>
2021-06-19 14:17:13 +0100

    ASSIGNMENT_BY_MARRYVERA_ENG1707213
    contains all the files for the assignment

---
+++ NWADIKE_C_MARRYVERA_ENG1707213/Marryvera_InterfaceCar/src/nmarvera/demoUseInterfaceCar.java
@@ -0,0 +1,38 @@
+package nmarvera;
+
+
+public class demoUseInterfaceCar implements demoInterfaceCar{
+
+    private int numberPassengers;
+    private double speed;
+
+    public demoUseInterfaceCar(int numberPassengers, double speed){
+
+        this.numberPassengers = numberPassengers;
+        this.speed = speed;
+
+    }
+
+    public int getNumberPassengers(){
+        return numberPassengers;
+    }
+
+    public boolean slowDown(double amount){
+        System.out.println("slowDown amount: " + amount);
+        return true;
+    }
+
+    public boolean speedUp(double amount){
+        System.out.println("speedUp amount: " + amount);
+        return true;
+    }
+
+    public double getSpeed(){
+        return speed;
+    }
+
+    public double getTooFastKPH(){
+        return demoInterfaceCar.TOO_FAST_KPH;
+    }
+
+}
