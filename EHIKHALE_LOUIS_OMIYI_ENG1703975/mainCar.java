c026902fac30205ec0f6d3bca30d7c526a67c44f
EHIKHALE <86146017+EHIKHALE@users.noreply.github.com>
2021-06-19 13:59:29 +0100

    ASSIGNMENT_BY_EHIKHALE_LOUIS_ENG1703975
    contains all my files on the assignment

---
+++ EHIKHALE_LOUIS_OMIYI_ENG1703975/mainCar.java
@@ -0,0 +1,17 @@
+package EHIKHALOM;
+
+public class mainCar {
+
+    public static void main(String[] args){
+
+        demoUseInterfaceCar newDemoCar = new demoUseInterfaceCar(7, 5.68);
+        newDemoCar.slowDown(22);
+        newDemoCar.speedUp(32);
+        newDemoCar.getNumberPassengers();
+        newDemoCar.getSpeed();
+        newDemoCar.getTooFastKPH();
+
+
+    }
+
+}
