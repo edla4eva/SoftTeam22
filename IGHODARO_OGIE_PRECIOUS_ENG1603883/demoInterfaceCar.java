253e188ca8b89196e16e48748589689764e62720
PreshDaro <86198759+PreshDaro@users.noreply.github.com>
2021-06-25 21:51:50 +0100

    Submitted Assignment

---
+++ IGHODARO_OGIE_PRECIOUS_ENG1603883/demoInterfaceCar.java
@@ -0,0 +1,8 @@
+
+public interface demoInterfaceCar
+{
+    public static final double TOO_FAST_KPH = 160.9;
+    //Methods in an interface are abstract w/ out using the keyword.
+    public boolean slowDown(double amount );
+    public boolean speedUp(double amount );
+}
